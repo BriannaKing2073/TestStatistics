7b363d6f062a2d92679af52d242c98dcd339969c
BriannaKing2073 <Brianna.KIng.2073@gmail.com>
2025-05-15 09:12:51 -0400

    Started test Statistics

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] testScores = {
+          85,90,96,65,45,89,75,74,64,83
+        };
+//will revisit
+        Arrays.binarySearch(testScores);
+
+        System.out.println("Hello, World!");
+    }
+
+
+}
